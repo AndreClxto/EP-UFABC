beee1af879d3844b23f4e0a4059c6d57b8607f2e
AndreClxto <andre.clxto@gmail.com>
2024-04-03 19:36:39 -0300

    Upload to Github

---
+++ andre-e-giovana/ep.py
@@ -0,0 +1,81 @@
+from funcoes import num_palavras, palavras_unicas, num_linhas, buscar_palavra, sub_palavras, imp_linha, frequencia_palavras
+import os
+
+endereço = "C:/Users/ndrca/OneDrive/Documents/UFABC Misc/ufabc python/andre-e-giovana/"
+os.chdir(endereço)
+
+print("Projeto Dirigido de André Inamura Calixto e Giovana Coutinho Nascimento \n")
+
+def main():
+    print("Insira um arquivo txt dentro da pasta 'andre-e-giovana', e escreva seu nome no terminal.\nEx: arquivo.txt\n")
+
+    while True:
+        arquivo = str(input())
+        arquivo_tipo = os.path.splitext(arquivo)[1]
+        if not os.path.exists(arquivo):
+            print(f"O arquivo '{arquivo}' não existe\nTente novamente.")
+        elif arquivo_tipo != ".txt":
+            print("O arquivo precisa ser do tipo txt\nTente novamente.\n")
+        else:
+            break
+
+    while True:
+        opcoes = int(input("\nDigite o número da opção desejada:\n\n1. Número de palavras\n2. Número de palavras distintas\n3. Número de linhas\n4. Frequência das palavras\n5. Imprimir uma linha específica\n6. Buscar uma palavra e imprimir a linha em que aparece a primeira ocorrência da palavra\n7. Substituir todas as ocorrências de uma palavra por outra\n8. Abrir outro livro\n9. Gerar nuvem de palavras\n10. Encerrar o programa\n"))
+
+        if opcoes == 1:
+            print(num_palavras(arquivo))
+
+        elif opcoes == 2:
+            print(palavras_unicas(arquivo))
+
+        elif opcoes == 3:
+            print(num_linhas(arquivo))
+        
+        elif opcoes == 4:
+            contagem_palavras = frequencia_palavras(arquivo)
+            if contagem_palavras:
+                print("Frequência das palavras:\n")
+                for palavra, quantidade in contagem_palavras.items():
+                    if quantidade == 1:
+                        print(f"A palavra {palavra} ocorre 1 vez;")
+                    else:
+                        print(f"A palavra {palavra} ocorre {quantidade} vezes;")
+            else:
+                print("O documento está vazio.")
+        
+        elif opcoes == 5:
+            indice_linha = int(input("Escreva o número da linha que você deseja imprimir: \n"))
+            print(imp_linha(arquivo, indice_linha))
+        
+        elif opcoes == 6:
+            busca = str(input("Escreva a palavra que você quer procurar.\n"))
+            maiuscula = busca.upper()
+            resultado = buscar_palavra(maiuscula, arquivo)
+            if resultado == False:
+                print(f"A palavra '{busca}' não foi achada no documento.")
+            else:
+                print(f"\n{resultado}")
+        
+        elif opcoes == 7:
+            nova = str(input("Escreva a palavra que você deseja adicionar ao documento:\n"))
+            antiga = str(input("Escreva a palavra que você deseja substituir no documento:\n"))
+            nome_arquivo = str(input("Escreva o nome do novo documento, sem extensão, em que você deseja salvar esse texto:\n"))
+            sub_palavras(nova, antiga, nome_arquivo, arquivo)
+            print(f"Todas as palavras '{antiga}', foram substituídas por '{nova}' no novo arquivo: {nome_arquivo}.txt")
+        
+        elif opcoes == 8:
+            main()
+            break
+        
+        # Andre precisa fazer
+        elif opcoes == 9:
+            print("Função em desenvolvimento")
+                
+        elif opcoes == 10:
+            print("Programa encerrado")
+            break
+
+        else:
+            print("Insira uma opção válida")
+        
+main()
---
+++ andre-e-giovana/funcoes.py
@@ -0,0 +1,79 @@
+# Referente a opção 1
+def num_palavras (arquivo):
+    quantidade_palavras = 0
+    f = open(arquivo, "r", encoding='utf-8-sig')
+    for linha in f:
+        palavras = linha.split()
+        quantidade_palavras += len(palavras)
+    if quantidade_palavras == 1:
+        return "O arquivo possui 1 palavra."
+    elif quantidade_palavras > 1:
+        return f"O arquivo possui {quantidade_palavras} palavras."
+    elif quantidade_palavras == 0:
+        return "O documento está vazio."
+
+# Referente a opção 2
+def palavras_unicas(arquivo):
+    conjunto_palavras = list()
+    f = open(arquivo, "r", encoding='utf-8-sig')
+    conjunto_linhas = f.readlines()
+    for linha in conjunto_linhas:
+        for palavra in linha.split():
+            palavra_limpa = palavra.strip(".,!?")
+            conjunto_palavras.append(palavra_limpa)
+    unicas = set(list(map(str.upper, conjunto_palavras)))
+    if len(unicas) > 1: 
+        return f"Há {len(unicas)} palavras distintas neste documento."
+    elif len(unicas) == 1:
+        return "Há 1 palavra distintas neste documento."
+    elif len(unicas) == 0:
+        return "O documento está vazio."
+
+# Referente a opção 3
+def num_linhas(arquivo):
+    f = open(arquivo, "r", encoding='utf-8-sig')
+    conjunto_linhas = f.readlines()
+    if conjunto_linhas > 1:
+        return f"Há {len(conjunto_linhas)} linhas neste documento."
+    elif conjunto_linhas == 0:
+        return "O documento está vazio."
+    
+# Referente a opção 4
+def frequencia_palavras(arquivo):
+    contagem_palavras = {}
+    f = open(arquivo, "r", encoding='utf-8-sig')
+    conjunto_linhas = f.readlines()
+    for linha in conjunto_linhas:
+        conjunto_palavras = linha.split()
+        for palavra in conjunto_palavras:
+            palavra_limpa = palavra.strip(".,!?")
+            if palavra_limpa:
+                contagem_palavras[palavra_limpa] = contagem_palavras.get(palavra_limpa, 0) + 1
+    return contagem_palavras
+
+# Referente a opção 5
+def imp_linha(arquivo, indice_linha):
+    f = open(arquivo, "r", encoding='utf-8-sig')
+    conjunto_linhas = f.readlines()
+    return conjunto_linhas[indice_linha-1]
+
+# Referente a opção 6
+def buscar_palavra(maiuscula: str, arquivo):
+    f = open(arquivo, "r", encoding='utf-8-sig')    
+    conjunto_linhas = f.readlines()
+    conjunto_palavras = list()
+    for linha in conjunto_linhas:
+        for palavra in linha.split():
+            conjunto_palavras.append(palavra.upper())
+            if palavra.upper() == maiuscula:
+                return linha
+    return False
+
+# Referente a opção 7
+def sub_palavras(nova, antiga, nome_arquivo, arquivo):
+    f = open(arquivo, "r+", encoding='utf-8-sig')
+    caminho_arquivo = f"{nome_arquivo}.txt"
+    novo_arquivo = open(caminho_arquivo, 'w', encoding='utf-8-sig')
+    texto = f.read()
+    novo_texto = texto.replace(antiga, nova)
+    novo_arquivo.write(novo_texto)
